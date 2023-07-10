380e0da71cef79c3a94b8d0f504102ecf436fbe1
Misaka <chenborun1101@gmail.com>
2023-07-09 23:03:56 -0400

    reverse_arr

--- lab1/reverse.c
+++ lab1/reverse.c
@@ -17,3 +17,17 @@
  * Given an array ["hello", "my", "name", "is"], after calling reverse_arr
  * will result in ["is", "name", "my", "hello"]
  */
+
+void swap(char** word_a, char** word_b) {
+  char* word_tmp = *word_a;
+  *word_a = *word_b;
+  *word_b = word_tmp;
+}
+
+
+void reverse_arr(char** arr, int num) {
+  for (int i = 0, j = num - 1; i < j; i ++ , j -- ) {
+    printf("%s %s\n", *(arr + i), *(arr + j));
+    swap(arr + i, arr + j);
+  }
+}
